72146c0bf83fd51c121c87762f5bade10c474c2e
Vukosi Mathebula <Vukosimathebula1100@gmail.com>
2023-09-20 22:13:48 +0200

    Adding a function that draws rectangles around detected objects and annotate it with an ID and object name

--- ultralytics/yolo/v8/detect/predict.py
+++ ultralytics/yolo/v8/detect/predict.py
@@ -93,6 +93,22 @@ def draw_border(img, pt1, pt2, color, thickness, r, d):
     
     return img
 
+#DRAW  RECTANGLE AROUND DETECTED OBJECT AND ANNOTATE WITH ID AND OBJECT NAME
+def UI_box(x, img, color=None, label=None, line_thickness=None):
+    # Plots one bounding box on image img
+    tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
+    color = color or [random.randint(0, 255) for _ in range(3)]
+    c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
+    cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+    if label:
+        tf = max(tl - 1, 1)  # font thickness
+        t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
+
+        img = draw_border(img, (c1[0], c1[1] - t_size[1] -3), (c1[0] + t_size[0], c1[1]+3), color, 1, 8, 2)
+
+        cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+
+
 
 class DetectionPredictor(BasePredictor):
 
